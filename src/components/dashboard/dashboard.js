54053a82b30249e8b66b4d8d3f50f6a12810e12b
Jorge Ordoñez <jorgeorm@gmail.com>
2019-02-25 20:36:28 -0500

    SETUP: Generating css bundle for production

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,15 +1,25 @@
-const JS_IMG_URL = 'https://www.tutorialrepublic.com/lib/images/javascript-illustration.png';
+import './dashboard.css';
 
+const JS_IMG_URL = 'https://www.tutorialrepublic.com/lib/images/javascript-illustration.png';
 
 export default ({ className }) => {
     const dashEl = document.createElement('div');
     const jsImgEl = document.createElement('img');
+    const paragraphEl = document.createElement('p');
+    const cardEl = document.createElement('div');
 
     jsImgEl.src = JS_IMG_URL;
-    jsImgEl.className = 'dashboard__img';
+    jsImgEl.className = 'card__img';
+
+    paragraphEl.className = 'card__desc';
+    paragraphEl.innerHTML = 'Fusce sed leo tempus, interdum ex eget, condimentum orci. Proin porttitor ligula eget ante tristique, quis feugiat nulla tempor. Nulla volutpat nulla ut tempor pretium. Nullam scelerisque, massa nec lacinia tincidunt, enim mauris sodales quam, ac vehicula nisl sem eget velit. Donec id facilisis nunc. Donec vel nibh in diam fermentum sollicitudin at et leo. Praesent quis odio eget justo rhoncus consequat vitae vel urna. Duis a varius quam. Aenean sollicitudin dolor eu laoreet pharetra. Aenean vel arcu semper, sagittis quam ut, condimentum diam. Duis augue tortor, placerat eu velit vitae, interdum lobortis leo.';
+
+    cardEl.className = 'card dashboard__card';
+    cardEl.appendChild(jsImgEl);
+    cardEl.appendChild(paragraphEl);
 
     dashEl.className = className ? `dashboard ${ className }` : 'dashboard';
-    dashEl.appendChild(jsImgEl);
+    dashEl.appendChild(cardEl);
 
     return dashEl;
 }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,3 +1,6 @@
-module.exports = {
-    
-}
+const wpMerge = require('webpack-merge');
+const styles = require('webpack-lib').styles;
+
+module.exports = wpMerge([
+    styles.extractCSS({ filename: '[name]-[hash:8].css' })
+]);
